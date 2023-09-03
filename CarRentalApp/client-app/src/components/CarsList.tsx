d89a93ddf735eedf78c0f8d70cb08c84ab8341c3
Tomasz Jaglarz <tomjaglarz@gmail.com>
2023-09-03 22:19:26 +0200

    added Cars display filter

--- CarRentalApp/client-app/src/components/CarsList.tsx
+++ CarRentalApp/client-app/src/components/CarsList.tsx
@@ -1,14 +1,14 @@
 import CarCard from './CarCard'
-import { useEffect, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import { Car } from '../common/types';
 import * as carApi from '../api/car/api';
 import AddNewCar from './AddNewCar';
 import { trackPromise, usePromiseTracker } from 'react-promise-tracker';
 import { PulseLoader } from 'react-spinners';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function CarsList() {
     const [cars, setCars] = useState<Car[]>([]);
+    const [filteredCars, setFilteredCars] = useState<Car[]>([]);
     const [toUpdate, setToUpdate] = useState(false);
     const { promiseInProgress } = usePromiseTracker();
 
@@ -19,9 +19,21 @@ function CarsList() {
     function GetCars() {
         trackPromise(
             carApi.GetAllCars()
-                .then(cars => setCars(cars))
+                .then(cars => {
+                    setCars(cars);
+                    setFilteredCars(cars)
+                })
                 .catch(error => console.log(error))
         );
+    }
+
+    const FilterCars = (event: ChangeEvent<HTMLInputElement>) => {
+        let filterQuery = event.target.value.toLowerCase();
+        if(filterQuery){
+            setFilteredCars(cars.filter(car => car.brand.toLowerCase().includes(filterQuery)));
+        }else{
+            setFilteredCars(cars);
+        }
     }
 
     return (
@@ -44,16 +56,13 @@ function CarsList() {
                 <AddNewCar setToUpdate={setToUpdate}></AddNewCar>
                 <div className="input-group">
                     <div className="form-outline">
-                        <input type="search" id="form1" className="form-control" placeholder='Search...' />
+                        <input type="search" id="form1" className="form-control" placeholder='Search...' onChange={FilterCars}/>
 
                     </div>
-                    <button type="button" className="btn btn-primary">
-                    <i className="fa fa-search"></i>
-                    </button>
                 </div>
             </div>
             <div className="row table">
-                {cars.map(car => (
+                {filteredCars.map(car => (
                     <div key={car.id} className="col-sm-4">
                         <CarCard car={car}></CarCard>
                     </div>
